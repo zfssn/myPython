44671288eef152310f4545e11515e69f92718df6
zhangfan <619779872@qq.com>
2018-04-03 21:06:34 +0800

    ceshi

--- 01.py
+++ 01.py
@@ -13,9 +13,9 @@ mingyue = Student()
 #定义一个学习Python的学生类
 class PythonStudent():
     #用None给不确定的变量赋值
-    name = None
+    name = 'zf'
     age = 18
-    course = 'Python'
+    course = ['Python','Java','Php']
 
 
     #定义个一个做功课的方法
@@ -24,8 +24,23 @@ class PythonStudent():
         return None
 
 #查看类的内容
-print(PythonStudent.__dict__)
-
+#print(PythonStudent.__dict__)
+print(PythonStudent.name)
+print(id(PythonStudent.name))
+print(PythonStudent.course)
+print(id(PythonStudent.course))
+print('*' * 20)
 #实例一个学生
 yueyue = PythonStudent()
-yueyue.doHomework()
+print(yueyue.name)
+print(id(yueyue.name))
+print(yueyue.course)
+print(id(yueyue.course))
+#yueyue.doHomework()
+print('*' * 20)
+a='zf'
+print(a)
+print(id(a))
+c=['Python','Java','Php']
+print(c)
+print(id(c))
